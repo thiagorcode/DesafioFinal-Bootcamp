61c86f778e50c9901a13a4f89e1a5e13831f3aa0
thiagorcode_n <54317829+thiagorcoden12@users.noreply.github.com>
2020-07-26 21:35:57 -0300

    heroku

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // !URL da API
 export default axios.create({
-   baseURL: 'http://localhost:3001/api/transaction',
+   baseURL: 'http://projetofinanceiro.herokuapp.com/api/transaction',
    headers: {
       'Content-type': 'application/json',
    },
